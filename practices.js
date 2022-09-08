6bfd19df3b159ddf5394f9535e595bb1424ae884
wahid30 <md.wahidulalamjp2@gmail.com>
2022-09-08 22:46:08 +0600

    first commit

---
+++ practices.js
@@ -0,0 +1,14 @@
+// problem 01
+// console.log(1);
+// console.log(2);
+// setTimeout(() => {
+//   console.log(3);
+// }, 1000);
+// console.log(4);
+// problem 02
+// problem 03
+// problem 04
+// problem 05
+// problem 06
+// problem 07
+// problem 08
